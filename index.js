780ca78df81b30142bec8f3fc3d20bf0a13b9b5f
Wes Byrne <wesburn@me.com>
2019-01-25 11:33:38 -0800

    change fetchKit options to string More documentation Add version option to outline fetch and search

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const Lingo = function () { }
 
 /**
  * Set your API auth credientials before using making any calls
+ * @param {integer} spaceID The id of your Lingo space
+ * @param {string} token An API token for your space
  */
-Lingo.prototype.setup = function (space_id, token) {
-    console.log(`space id: ${space_id} token: ${token}`)
-    this.auth = "Basic " + new Buffer(space_id + ":" + token).toString("base64")
+Lingo.prototype.setup = function (spaceID, token) {
+    console.log(`space id: ${spaceID} token: ${token}`)
+    this.auth = "Basic " + new Buffer(spaceID + ":" + token).toString("base64")
 }
 
 /**
@@ -23,10 +25,15 @@ Lingo.prototype.fetchKits = function () {
 
 /**
  * Fetch a single kit and it's versions
+ * @param {uuid} id the kit uuid
+ * @param {string} include optionally include versions of the kit. valid options are:
+ * - `use_versions`: include the draft and recommended version (if different)
+ * - `versions`: include all versions
+ * - `null`: don't include any versions
  * @returns {Promise} Success returns a kit
  */
-Lingo.prototype.fetchKit = function (id, options = ['use_versions']) {
-    let path = `/kits/${id}/?options=${options.join(',')}`
+Lingo.prototype.fetchKit = function (id, include = 'use_versions') {
+    let path = `/kits/${id}/?options=${include}`
     return this.call('GET', path).then(res => {
         return res.kit
     })
@@ -34,10 +41,12 @@ Lingo.prototype.fetchKit = function (id, options = ['use_versions']) {
 
 /**
  * Fetch the outline for a kit
+ * @param {uuid} id the kit uuid
+ * @param {integer} version the version number of the kit to fetch
  * @returns {Promise} Success returns a list of sections and headers
  */
-Lingo.prototype.fetchKitOutline = function (id) {
-    let path = `/kits/${id}/outline`
+Lingo.prototype.fetchKitOutline = function (id, version) {
+    let path = `/kits/${id}/outline?v=${version}`
     return this.call('GET', path).then(res => {
         return res.kit_version.sections
     })
@@ -45,11 +54,16 @@ Lingo.prototype.fetchKitOutline = function (id) {
 
 /**
  * Fetch a section and optionally page through items with it
+ * @param {uuid} id the section uuid
+ * @param {integer} version the version number of the section to fetch
+ * @param {integer} limit The max number of items to fetch
+ * @param {integer} page the page of items
  * @returns {Promise} Success the section and the items matching the page/limit
  */
-Lingo.prototype.fetchSection = function (id, page = 1, limit = 50) {
+Lingo.prototype.fetchSection = function (id, version, page = 1, limit = 50) {
     let path = `/sections/${id}`
-    let params = { qs: { page, limit } }
+    let v = version
+    let params = { qs: { v, page, limit } }
     return this.call('GET', path, params).then(res => {
         return res.section
     })
@@ -57,12 +71,17 @@ Lingo.prototype.fetchSection = function (id, page = 1, limit = 50) {
 
 /**
  * Search the items in a kit.
- *
+ * @param {uuid} kitID The uuid of the kit to search
+ * @param {integer} version The version to search
+ * @param {string} query A search query to filter by
+ * @param {integer} page For paging resutls
+ * @param {integer} limit The max number of results per page
  * @returns {Promise} Returns the results, grouped by section.
  */
-Lingo.prototype.searchAssetsInKit = function (kitID, query, page, limit) {
-    let path = `/kits/${kitID}/search`
-    let params = { qs: { query, page, limit } }
+Lingo.prototype.searchAssetsInKit = function (kitID, version, query, page, limit) {
+    const path = `/kits/${kitID}/search`
+    const v = version
+    let params = { qs: { v, query, page, limit } }
     return this.call('GET', path, params)
 }
 

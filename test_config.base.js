ae9b16e5f454758fe4c899b9105318be41b793aa
Wes Byrne <wesburn@me.com>
2019-01-28 09:29:15 -0800

    Update test config comments

--- test_config.base.js
+++ test_config.base.js
@@ -1,7 +1,7 @@
-// Copy this file and rename it to test_config.js
-// These attributes are used to run tests againts the actual API
+// Copy this file and rename test_config.js
+// These values are required by tests to run againts the actual API
 module.exports = {
-    spaceID: 0, // set spaceID
+    spaceID: 0,
     apiToken: '',
 
     kitID: '',
